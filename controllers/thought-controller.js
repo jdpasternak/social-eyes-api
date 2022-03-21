d32079050a3c9cf5a15d0ac7fc1986a7798a370b
Jake Pasterank <jdp.pasternak@gmail.com>
2022-03-20 18:33:52 -1000

    sets up specific thought controller functions with related routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,33 @@
 const { Thought } = require("../models");
 
-const thoughtController = {};
+const thoughtController = {
+  getAllThought(req, res) {
+    res.send("getAllThought");
+  },
+
+  getThoughtById({ params }, res) {
+    res.send("getThoughtById");
+  },
+
+  createThought({ body }, res) {
+    res.send("createThought");
+  },
+
+  updateThought({ params, body }, res) {
+    res.send("updateThought");
+  },
+
+  deleteThought({ params }, res) {
+    res.send("deleteThought");
+  },
+
+  addReaction({ params, body }, res) {
+    res.send("addReaction");
+  },
+
+  removeReaction({ params }, res) {
+    res.send("removeReaction");
+  },
+};
 
 module.exports = thoughtController;
